ef41a79b54b3f80c65c8fce3f155d72a80c65363
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2024-05-26 12:46:13 +0530

    Setup Health Checks for API and SQL Server

--- src/UrlShortener/Program.cs
+++ src/UrlShortener/Program.cs
@@ -2,15 +2,20 @@ using UrlShortener;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString(
+    StorageConstants.DEFAULT_CONNECTION_NAME
+);
 builder.Host.UseOrleans(siloBuilder =>
 {
     siloBuilder.UseLocalhostClustering();
     siloBuilder.AddAdoNetGrainStorageAsDefault(options =>
     {
         options.Invariant = StorageConstants.DEFAULT_STORAGE_INVARIANT;
-        options.ConnectionString = builder.Configuration.GetConnectionString(StorageConstants.DEFAULT_CONNECTION_NAME);
+        options.ConnectionString = connectionString;
     });
 });
+
+builder.Services.AddHealthChecks().AddSqlServer(connectionString);
 
 builder.Services.AddControllers();
 
@@ -18,6 +23,7 @@ using var app = builder.Build();
 
 app.UseRouting();
 app.MapControllers();
+app.MapHealthChecks("/healthz");
 app.MapGet("/", () => "URL Shortner using Microsoft Orleans");
 
 app.Run();
